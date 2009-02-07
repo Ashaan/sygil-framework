5387995b4e7b9311cd13152addd5f22c44e48722
root <root@aegis.sygil.org>
2009-02-07 23:54:00 +0100

    Add english

--- langue/english.php
+++ langue/english.php
@@ -1,8 +1,34 @@
 <?php
 
-$lang['site_short_name']	= SITE_SHORT_NAME;
-$lang['welcome']		= 'Welcome on '.SITE_SHORT_NAME.' !';
-$lang['hello']			= 'Hi';
+$lang['site_short_name']	= defined(SITE_SHORT_NAME_EN)  ? SITE_SHORT_NAME_EN  : SITE_SHORT_NAME;
+$lang['site_long_name']		= defined(SITE_LONG_NAME_EN)   ? SITE_LONG_NAME_EN   : SITE_LONG_NAME;
+$lang['site_description']	= defined(SITE_DESCRIPTION_EN) ? SITE_DESCRIPTION_EN : SITE_DESCRIPTION;
+$lang['site_keywords']		= SITE_SEARCH_KEYWORDS.'';
 
+$lang['msg_ie6'] = 'Ce site ne fonctionne pas avec Internet Explorer 6-\n'.
+                   '\n'.
+                   'Mettez a jour Windows ou installer un navigateur fiable :\n'.
+		   '  - Mozilla Firefox (pour un rendu optimal)\n'.
+        	   '  - Google Chrome\n'.
+                   '  - Opera\n'.
+                   '  - Safari\n';
+$lang['msg_ns6'] = 'Ce site ne fonctionne pas avec Netscape 6-\n'.
+		   '\n'.
+		   'Installer un navigateur plus recent :\n'.
+		   '  - Mozilla Firefox (pour un rendu optimal)\n'.
+        	   '  - Google Chrome\n'.
+                   '  - Opera\n'.
+                   '  - Safari\n';
+
+$lang['menu_Home']		= 'Home';
+$lang['menu_Project']		= 'Project';
+
+$lang['Welcome_on']		= 'Welcome on';
+$lang['Hello']			= 'Hi';
+
+$lang['Option']      		= 'Option';
+$lang['Register']      		= 'Register';
+$lang['Connect']    		= 'Connect';
+$lang['Disconnect'] 		= 'Disconnect';
 
 ?>

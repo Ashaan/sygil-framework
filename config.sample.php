710622f62a4a7d75fc4b58e59eccda06b2d8362d
Mathieu Chocat <mathieu@chocat.com>
2009-02-09 23:59:14 +0100

    Ajout de commentaire

--- config.sample.php
+++ config.sample.php
@@ -10,14 +10,20 @@
 foreach($_POST as $name => $value) {
     $_GET[$name] = $value;
 }
+
+// Page par default
 if (!isset($_GET['Frame']) && !isset($_GET['Ajax'])) {
     $_GET['Ajax'] = 'blog';
+    //$_GET['frame'] = 'http://www.google.com/';
 }
 
+// Permet d'indiquer que le site est deja configurer
 define('CONFIGURE',true);
+// Permet d'indiquer que le site a deja subit le script d'installation (innexistant)
 define('INSTALL',true);
 
-define('PATH_CORE'              , '/data/www/sygil/www/core');
+// Definit les repertoire d'installation du CORE
+define('PATH_CORE'              , './core');
 define('PATH_TEMPLATE'          , PATH_CORE.'/template'); 
 define('PATH_THEME'             , PATH_CORE.'/theme'); 
 define('PATH_ICON'              , PATH_CORE.'/icon'); 
@@ -32,9 +38,12 @@ define('PATH_MODULE_INCLUDE'    , PATH_MODULE_.'/include');
 define('PATH_MODULE_TEMPLATE'   , PATH_MODULE_.'/template'); 
 define('PATH_MODULE_THEME'      , PATH_MODULE_.'/theme'); 
 define('PATH_MODULE_SCRIPT'     , PATH_MODULE_.'/script'); 
-define('PATH_ZONE'              , '/data/www/sygil/www/zone'); 
 
-define('URL_CORE'               , 'http://www.sygil.org/core');
+// Definit les repertoire d'installation des infos de votre site
+define('PATH_ZONE'              , './zone'); 
+
+// URL a utiliser pour acceder au CORE (pour les script et les themes, peut etre un site distant
+define('URL_CORE'               , 'core');
 define('URL_SCRIPT'             , URL_CORE.'/script');
 define('URL_THEME'              , URL_CORE.'/theme');
 /************************* Ne pas Editer ************************/
@@ -43,9 +52,22 @@ define('URL_THEME'              , URL_CORE.'/theme');
 define('URL_MODULE_SCRIPT'      , URL_MODULE_.'/script');
 define('URL_MODULE_THEME'       , URL_MODULE_.'/theme');
 define('URL_ICON'               , URL_CORE.'/icon');
-define('URL_ZONE'               , 'http://www.sygil.org/zone');
+
+// URL de votre site
+define('URL_ZONE'               , 'http://www.sygil.org');
 define('URL_CACHE'              , URL_ZONE.'/cache');
 define('URL_DATA'               , URL_ZONE.'/data');
+
+// Paramatre par defaut
+define('DEFAULT_LANGUE'  , 'french');
+define('DEFAULT_THEME'   , 'glossy');
+define('DEFAULT_TEMPLATE', 'sygil');
+define('DEFAULT_ICON'	 , 'dropline-neu');
+
+// Contenu d'affichage par default
+define('DEFAULT_LEFT'    , 'left');
+define('DEFAULT_CENTER'  , null);
+define('DEFAULT_RIGHT'   , null);
 
 
 //Name of your site

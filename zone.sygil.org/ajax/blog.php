195aed06179e18826a6972f2698e7d2a88590798
Mathieu Chocat <mathieu@chocat.com>
2009-02-13 00:05:45 +0100

    Implement of slide_block

--- zone.sygil.org/ajax/blog.php
+++ zone.sygil.org/ajax/blog.php
@@ -18,14 +18,14 @@ $right->setBottom(  '0px');
 $right->setRight (  '0px');
 $right->setWidth  ('200px');
 $right->addLine('panel_right_row1');
-$line = $right->addLine('panel_right_row2');
-$line->setBackground('#6798ca');
+//$line = $right->addLine('panel_right_row2');
+//$line->setBackground('#6798ca');
 $line = $right->addLine('panel_right_row3');
 $line->setBackground('#6798ca');
 
 $core->addExec('ajax.load(\'news_latest\',\'panel_left\',\'replace\',[[\'module\',\'14a6a157d8e861359729faeb72f5ca17\']]);');
 $core->addExec('ajax.load(\'calendar\',\'panel_right_row1\',\'replace\',[[\'module\',\'14a6a157d8e861359729faeb72f5ca17\']]);');
-$core->addExec('ajax.load(\'lastfm_player\',\'panel_right_row2\',\'replace\',[]);');
+//$core->addExec('ajax.load(\'lastfm_player\',\'panel_right_row2\',\'replace\',[]);');
 $core->addExec('ajax.load(\'lastfm_latest\',\'panel_right_row3\',\'replace\',[]);');
 
 $core->setData('__NAME__','Accueil');
---
+++ zone.sygil.org/index/right.php
@@ -0,0 +1,24 @@
+<?php
+
+$core = Core::getInstance();
+$core->loadModule('slide_block');
+$core->loadModule('panel');
+
+$slide = new SlideBlock();
+
+$panel = new Panel('panel_slide');
+$panel->setTop   (  '0px');
+$panel->setBottom(  '0px');
+$panel->setLeft  (  '0px');
+$panel->setRight (  '0px');
+$panel->setOverflow ('none');
+$row = $panel->addLine('panel_slide_row1');
+$row->setBackground('#6798ca');
+
+$slide->addBlock($panel);
+
+$core->addScriptInit ('ajax.load(\'lastfm_player\',\'panel_slide_row1\',\'replace\',[]);');
+$core->addScriptInit ('slide_block.width = \'210px\';');
+
+$core->setData('__RIGHT__', $slide->generate());
+?>
